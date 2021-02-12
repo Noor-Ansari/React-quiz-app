a40e5cedbb7e0774ec490f682efd920e9c65581d
Noor Coder <70513695+Noor-Ansari@users.noreply.github.com>
2021-02-12 17:21:17 +0530

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,18 +4,14 @@ import "./Home.css"
 
 function Home(){
 	const [topics, setTopics] = useState(["Science", "Computer Science", "Technology", "Geography", "Programming"])
-
 	return(
 		<div>
 			<h1> Go ahead and test your intelligence</h1>
 			<div className="wrapper">
 				{topics.map(topic => (
-					<Link className="link" to={topic} ><div className="card"><p>{topic}</p></div></Link>
-					)
-				)}
+				<Link className="link" to={topic} ><div className="card"><p>{topic}</p></div></Link>))}
 			</div>
 		</div>
-	)
-}
+	)}
 
 export default Home;
